15ff15e02daf6c5ceddef8885f9e5293dbe7b33a
Michael Liao <askxuefeng@gmail.com>
2017-01-03 09:41:22 +0800

    add bitcoin exception

---
+++ bitcoin-common/src/main/java/com/itranswarp/bitcoin/BitcoinException.java
@@ -0,0 +1,19 @@
+package com.itranswarp.bitcoin;
+
+public class BitcoinException extends RuntimeException {
+
+	public BitcoinException() {
+	}
+
+	public BitcoinException(String message) {
+		super(message);
+	}
+
+	public BitcoinException(Throwable cause) {
+		super(cause);
+	}
+
+	public BitcoinException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
