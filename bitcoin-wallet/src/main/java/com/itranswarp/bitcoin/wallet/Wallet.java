83415038e5bd5323ef3b84ac69b498c7742c7c26
Michael Liao <askxuefeng@gmail.com>
2017-01-12 19:48:52 +0800

    add swing ui

--- bitcoin-wallet/src/main/java/com/itranswarp/bitcoin/wallet/Wallet.java
+++ bitcoin-wallet/src/main/java/com/itranswarp/bitcoin/wallet/Wallet.java
@@ -1,9 +1,34 @@
 package com.itranswarp.bitcoin.wallet;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+
 public class Wallet {
 
 	public Wallet() {
 		// TODO Auto-generated constructor stub
 	}
 
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});
+	}
+
+	static void createAndShowGUI() {
+		// Create and set up the window.
+		JFrame frame = new JFrame("Bitcoin Wallet");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		// Add the ubiquitous "Hello World" label.
+		JLabel label = new JLabel("Hello World");
+		frame.getContentPane().add(label);
+
+		// Display the window.
+		frame.pack();
+		frame.setVisible(true);
+	}
 }
