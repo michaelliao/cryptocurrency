036c4e766f99474a826d96f03e91eaccaba03fef
Michael Liao <askxuefeng@gmail.com>
2017-02-15 10:22:49 +0800

    add getResources under classpath

--- bitcoin-common/src/main/java/com/itranswarp/bitcoin/util/ClasspathUtils.java
+++ bitcoin-common/src/main/java/com/itranswarp/bitcoin/util/ClasspathUtils.java
@@ -1,8 +1,14 @@
 package com.itranswarp.bitcoin.util;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClasspathUtils {
 
@@ -18,6 +24,33 @@ public class ClasspathUtils {
 				output.write(buffer, 0, n);
 			}
 			return output.toByteArray();
+		}
+	}
+
+	/**
+	 * Get resource names under specific path.
+	 * 
+	 * @param path
+	 * @param filter
+	 * @return
+	 * @throws IOException
+	 */
+	public static List<String> getResources(String path, FilenameFilter filter) throws IOException {
+		try (InputStream input = ClasspathUtils.class.getResourceAsStream(path)) {
+			if (input == null) {
+				throw new IOException("Resource " + path + " not found in classpath.");
+			}
+			try (BufferedReader br = new BufferedReader(new InputStreamReader(input, "UTF-8"))) {
+				String resource = null;
+				File dir = new File(path);
+				List<String> filenames = new ArrayList<>();
+				while ((resource = br.readLine()) != null) {
+					if (filter.accept(dir, resource)) {
+						filenames.add(resource);
+					}
+				}
+				return filenames;
+			}
 		}
 	}
 }
--- bitcoin-common/src/test/java/com/itranswarp/bitcoin/util/ClasspathUtilsTest.java
+++ bitcoin-common/src/test/java/com/itranswarp/bitcoin/util/ClasspathUtilsTest.java
@@ -2,7 +2,10 @@ package com.itranswarp.bitcoin.util;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -15,4 +18,22 @@ public class ClasspathUtilsTest {
 		assertTrue(txt.contains("org.apache.commons.logging.Log"));
 	}
 
+	@Test
+	public void testGetResources() throws IOException, ClassNotFoundException {
+		String pkg = "com.itranswarp.bitcoin.util.sub";
+		List<String> cls = ClasspathUtils.getResources("/" + pkg.replace('.', '/'), new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				return name.endsWith(".class");
+			}
+		});
+		assertEquals(3, cls.size());
+		assertTrue(cls.contains("A.class"));
+		assertTrue(cls.contains("B.class"));
+		assertTrue(cls.contains("C.class"));
+		// try load classes:
+		Class.forName(pkg + "." + "A");
+		Class.forName(pkg + "." + "B");
+		Class.forName(pkg + "." + "C");
+	}
 }
