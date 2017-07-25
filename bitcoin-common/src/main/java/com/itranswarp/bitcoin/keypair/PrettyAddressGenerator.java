a3f56739c1021c3071385047846a0555a26b9d8a
Michael Liao <askxuefeng@gmail.com>
2017-07-25 13:40:13 +0800

    add comment

--- bitcoin-common/src/main/java/com/itranswarp/bitcoin/keypair/PrettyAddressGenerator.java
+++ bitcoin-common/src/main/java/com/itranswarp/bitcoin/keypair/PrettyAddressGenerator.java
@@ -2,17 +2,30 @@ package com.itranswarp.bitcoin.keypair;
 
 import java.util.Scanner;
 
+/**
+ * Generate pretty address starts with specific word. e.g. "BTC".
+ * 
+ * For example:
+ * 
+ * public key: 1BTC48PCwMQMhKr79xgjwdFJFe84iBTrdF private key:
+ * Kzgnp4nUukjYuDuJBp9h1hSW4JVigyCAzUXVNvbx2MsLAWUwug5n
+ * 
+ * @author Michael Liao
+ */
 public class PrettyAddressGenerator {
+
+	static final long NUM_OF_ADDR = 10;
 
 	public static void main(String[] args) {
 		try (Scanner scanner = new Scanner(System.in)) {
-			System.out.print("Enter the prefix (e.g. Bob): ");
+			System.out.print("Enter the prefix (e.g. BTC): ");
 			String prefix = "1" + scanner.nextLine();
 			System.out.println("Find address starts with prefix: " + prefix);
+			final long step = 1 << Math.min(60, prefix.length() * 11);
 			long n = 0;
 			long found = 0;
 			for (;;) {
-				if (n % 10000 == 0) {
+				if (n % step == 0) {
 					System.out.print('.');
 				}
 				n++;
@@ -23,9 +36,13 @@ public class PrettyAddressGenerator {
 					found++;
 					n = 0;
 					System.out.println("\n" + caddr + " " + cwif);
-					if (found == 10) {
+					if (found == NUM_OF_ADDR) {
 						break;
 					}
+				}
+				if (n == Long.MAX_VALUE) {
+					System.out.println("Ooops NO ADDRESS FOUND!");
+					break;
 				}
 			}
 			System.out.println("Found " + found + " addresses.");
